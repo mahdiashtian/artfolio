9aa4e999edb97062c4a8108349bfca3f898078dd
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-03-01 17:38:02 +0330

    Add condition to send_notification_to_user function

--- activities/signals.py
+++ activities/signals.py
@@ -10,8 +10,9 @@ User = get_user_model()
 
 @receiver(post_save, sender=ArtActivity)
 def send_notification_to_user(sender, instance: ArtActivity, created: bool, **kwargs):
-    user: User = instance.user
-    if user.phone_number:
-        send_sms.delay(user.phone_number, f'فعالیت {instance.name} با موفقیت ثبت شد.')
-    if user.email:
-        send_email.delay(user.email, f'فعالیت {instance.name} با موفقیت ثبت شد.')
+    if created:
+        user: User = instance.user
+        if user.phone_number:
+            send_sms.delay(user.phone_number, f'فعالیت {instance.name} با موفقیت ثبت شد.')
+        if user.email:
+            send_email.delay(user.email, f'فعالیت {instance.name} با موفقیت ثبت شد.')
