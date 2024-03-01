575be79ed27d1ab5e7a317c424751868120281c6
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-03-01 17:04:57 +0330

    Create utility

---
+++ utils/file.py
@@ -0,0 +1,42 @@
+import os
+
+from django.utils.crypto import get_random_string
+
+
+def get_file_name(file_name):
+    """
+    _Get File Name_
+    Args:
+        file_name (_str_): Name file (image,movie and ...)
+
+    Returns:
+        _tuple_: Name and ext
+
+    Example:
+        >>> get_file_name('name.ext')
+        ('name', '.ext')
+    """
+
+    base_name = os.path.basename(file_name)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance=None, filename=None):
+    """
+    _Upload Image Path_
+    Args:
+        instance (django obj instance, optional): The object that the file is for. Defaults to None.
+        filename (_type_, optional): The final generated name for the file. Defaults to None.
+
+    Returns:
+        _str_: Final path and file name
+
+    Example:
+        >>> upload_image_path(instance, filename)
+        'model_name/random_string/name.ext'
+    """
+
+    model_name = instance.__class__.__name__.lower()
+    name, ext = get_file_name(filename)
+    return f"{model_name}/{get_random_string(10)}/{name}{ext}"
